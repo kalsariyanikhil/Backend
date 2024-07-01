1f46c89b7ba6a808be69d1cd1cab0349f5f15f01
nikhil kalsariya <nickahir0786@gmail.com>
2024-07-01 08:37:22 +0530

    Basic

--- 1.SQL/Lecture1/lecture1.sql
+++ 1.SQL/Lecture1/lecture1.sql
@@ -23,3 +23,37 @@
 8.describe or show perticular table(row and column)
 -- desc user;
 -- describe user;
+
+
+
+
+
+-- insert data 
+
+insert into user (firstname , lastname , age) values ('jhon' , 'peter', 22);
+
+-- multipel type data pssing
+
+insert into user (firstname , lastname , age) values 
+                 ('Nikhil' , 'Ahir', 26),
+                 ('Jayan' , 'Patel', 25),
+                 ('Akshay' , 'Goti', 24),
+                 ('Sahil' , 'Swami', 23);
+
+
+insert into user  values ('Ketan' , 'Ahir', 27),
+                 
+insert into user (firstname , lastname ) values ('Shivam' , 'Dube');
+insert into user (firstname , age ) values ('Virat' , 35);
+
+
+
+
+-- Read Data 
+select * from user;
+select firstname from user;
+select firstname , age from user;
+select firstname as "Full Name" , age from user;
+select firstname as "Hello" , age from user;
+select * , age from user ;
+select concat(firstname , "" , lastname ) as "full name" , age from user;
