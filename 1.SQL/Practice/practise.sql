c322817a4d899582ee2dee621d0b4c9235cc9db0
nikhil kalsariya <nickahir0786@gmail.com>
2024-07-06 09:53:24 +0530

    basic

--- 1.SQL/Practice/practise.sql
+++ 1.SQL/Practice/practise.sql
@@ -2,12 +2,16 @@
 select * from customers;
 
 -- 2. Fetch & display employee details for the employees having job Title='Sales Rep' and Employee number between 1200 and 1500
- SELECT * FROM employees WHERE jobTitle = "Sales Rep" AND employeeNumber BETWEEN 1200 AND 1500;
+ SELECT * FROM employees WHERE jobTitle = "Sales Rep" AND employeeNumber between 1200 AND 1500;
+ 
+-- 3. Fetch & display employee details for the employees having office code 2 or more than 2 and job title is not Sales Rep
+select * from employees where officeCode>=2 and jobTitle!="salesrep";
 
 -- 4. Fetch & display customers details(Customers table) who does not belongs to USA
 SELECT * FROM customers WHERE country != "USA";
 
 -- 5. Fetch & display customers details(Customers table)  having customer number not in the range 150 to 200
+select * from customers where customerNumber not in (150,200);
 
 -- 6. List out last name, first name for all employees and rename  last name as “Name of the employee”, display as sorting order of their last name
  SELECT firstName,lastName as Name FROM employees order by lastName ;
@@ -22,11 +26,13 @@ SELECT * FROM customers WHERE city = "Singapore" || city = "Liverpool" || city =
 SELECT * FROM customers WHERE contactFirstName IN ("SMITH");
 
 -- 10. List out the employees who are working in office code 1, 3 , 5
+select * from employees where officeCode in(1,3,5);
 
 -- 11. List out the customer having credit limit between 50000 and 95000
 SELECT * FROM customers WHERE creditLimit BETWEEN 50000 AND 95000;
 
 -- 12. List out the employees who are not working in office code 1 , 3
+select * from employees where officeCode not in(1,3);
 
 -- 13. List out the customers whose first name starts with “S”
 SELECT * FROM customers WHERE contactFirstName LIKE "S%";
@@ -38,15 +44,17 @@ SELECT * FROM customers WHERE contactFirstName LIKE "S%H";
 SELECT * FROM customers WHERE contactFirstName LIKE "s%____";
 
 -- 16. List out the customers whose first name length is exactly 8
-SELECT * FROM customers WHERE contactFirstName LIKE "_________";
+SELECT * FROM customers WHERE contactFirstName LIKE "________ ";
 
 -- 17. List out the customers whose first name starting with a and ending with e
+SELECT * FROM customers WHERE contactFirstName LIKE ("a%e ");
 
 -- 18. List out the customers whose first name contains ee
 SELECT * FROM customers WHERE contactFirstName LIKE "%e";
 
 -- 19. Show Count of employees who are working in different office in the organization
+select officecode,count(*) from employees group by officeCode;
 
 -- 20. List out the maximum credit limit of customers belongs to different states
-
+select max(creditlimit) from customers group by state;
 
